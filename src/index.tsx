783aa39004f5ee5984aaa970be1fff19bb8ad8c5
thieryw <williamalexthiery@hotmail.fr>
2021-10-17 17:35:08 +0200

    set up basic gitlanding page

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,102 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from "react-dom";
+import { GlTemplate } from "gitlanding/GlTemplate";
+import { GlHeader } from "gitlanding/GlHeader";
+import { GlHero } from "gitlanding/GlHero";
+import { GlArticle } from "gitlanding/GlArticle";
+import { GlIllustration } from "gitlanding/GlIllustration";
+import { GlFooter } from "gitlanding/GlFooter";
+import {GlSectionDivider} from "gitlanding/GlSectionDivider";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  return (
+    <GlTemplate
+      header={
+        <GlHeader
+          title="Header title"
+          links={[
+            {
+              "label": "link 1",
+              "link": { "href": "https://example.com" },
+            },
+            {
+              "label": "link 2",
+              "link": { "href": "https://example.com" },
+            },
+            {
+              "label": "link 3",
+              "link": { "href": "https://example.com" },
+            },
+          ]}
+          enableDarkModeSwitch={true}
+          githubRepoUrl="https://github.com/torvalds/linux"
+          githubButtonSize="large"
+        />
+      }
+      headerOptions={{
+        "position": "fixed",
+        "isRetracted": "smart",
+      }}
+      footer={
+        <GlFooter 
+          bottomDivContent="Licence M I T"
+          email="email@email.com"
+          phoneNumber="+33545345676"
+          links={[
+            {
+              "href": "https://example.com",
+              "title": "link 1"
+            },
+            {
+              "href": "https://example.com",
+              "title": "link 2"
+            },
+            {
+              "href": "https://example.com",
+              "title": "link 3"
+            },
+          ]}
+        />
+      }
+    >
+      <GlHero
+        title="Hero title"
+        subTitle={"Hero subtitle"}
+        imageSrc="https://user-images.githubusercontent.com/39378411/135731749-4a723d4e-52ea-49b7-83c1-7da4db8f3f59.png"
+        linkToSectionBelowId="firstSection"
+      />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      <GlArticle
+        id="firstSection"
+        title="Article title"
+        body="Article body"
+        buttonLabel="Article Button label"
+        buttonLink={{
+          "href": "https://example.com",
+        }}
+        illustration={
+          <GlIllustration type="image" url="https://user-images.githubusercontent.com/39378411/135731808-6cf3e4dd-1047-4a0a-95be-65fdd6947315.png" />
+        }
+        animationVariant="primary"
+        illustrationPosition="left"
+      />
+
+      <GlSectionDivider />
+
+      <GlArticle
+        title="Article title"
+        body="Article body"
+        buttonLabel="Article Button label"
+        buttonLink={{
+          "href": "https://example.com",
+        }}
+        illustration={
+          <GlIllustration type="image" url="https://user-images.githubusercontent.com/39378411/135731816-5ba39459-d95e-413d-b515-92a7b0dc5acf.png" />
+        }
+        animationVariant="secondary"
+      />
+
+    </GlTemplate>
+  );
+}
+
+render(<App />, document.getElementById("root"));
