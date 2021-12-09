7a2003d285d8f3a6b2d5391474af5cc691f03910
thieryw <williamalexthiery@hotmail.fr>
2021-12-09 14:47:56 +0100

    Using multiple fonts

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,32 @@
 import { createThemeProvider, defaultGetTypographyDesc } from "onyxia-ui";
 
 export const { ThemeProvider, useTheme } = createThemeProvider({
-	"getTypographyDesc": params => ({
-		...defaultGetTypographyDesc(params),
-		"fontFamily": '"Work Sans", sans-serif'
-	})
+	"getTypographyDesc": ({
+		browserFontSizeFactor, 
+		windowInnerHeight, 
+		windowInnerWidth
+	}) => {
+
+		const typographyDesc = defaultGetTypographyDesc({
+			browserFontSizeFactor,
+			windowInnerHeight, 
+			windowInnerWidth
+		})
+
+		return {
+			"fontFamily": '"Work Sans", sans-serif',
+			"rootFontSizePx": typographyDesc.rootFontSizePx,
+			"variants": {
+				...typographyDesc.variants,
+				"page heading": {
+					...typographyDesc.variants["page heading"],
+					"fontFamily": '"Playfair Display", serif'
+				},
+				"subtitle": {
+					...typographyDesc.variants["subtitle"],
+					"fontFamily": '"Open Sans", sans-serif'
+				}
+			}
+		}
+	}
 }); 
