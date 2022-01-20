2ef142209c932f0bbef68b271ee682908ab0e2af
thieryw <williamalexthiery@hotmail.fr>
2022-01-20 15:41:42 +0100

    update links and include pages in GlTemlate

--- src/index.tsx
+++ src/index.tsx
@@ -3,65 +3,81 @@ import { GlTemplate } from "gitlanding/GlTemplate";
 import { GlHeader } from "gitlanding/GlHeader";
 import { GlFooter } from "gitlanding/GlFooter";
 import { ThemeProvider } from "./theme";
+import { RouteProvider, useRoute, routes } from "./router";
+import { Home } from "./pages/Home";
+import { Page1 } from "./pages/Page1";
+import { Page2 } from "./pages/Page2";
+import { Page3 } from "./pages/Page3";
 
 function App() {
-  return (
-    <GlTemplate
-      ThemeProvider={ThemeProvider}
-      header={
-        <GlHeader
-          title="Header title"
-          links={[
-            {
-              "label": "link 1",
-              "link": { "href": "https://example.com" },
-            },
-            {
-              "label": "link 2",
-              "link": { "href": "https://example.com" },
-            },
-            {
-              "label": "link 3",
-              "link": { "href": "https://example.com" },
-            },
-          ]}
-          enableDarkModeSwitch={true}
-          githubRepoUrl="https://github.com/torvalds/linux"
-          githubButtonSize="large"
-        />
-      }
-      headerOptions={{
-        "position": "fixed",
-        "isRetracted": "smart",
-      }}
-      footer={
-        <GlFooter
-          bottomDivContent="Licence M I T"
-          email="email@email.com"
-          phoneNumber="+33545345676"
-          links={[
-            {
-              "href": "https://example.com",
-              "title": "link 1"
-            },
-            {
-              "href": "https://example.com",
-              "title": "link 2"
-            },
-            {
-              "href": "https://example.com",
-              "title": "link 3"
-            },
-          ]}
-        />
-      }
-    >
-
-    </GlTemplate>
-  );
+    const route = useRoute();
+    return (
+        <GlTemplate
+            ThemeProvider={ThemeProvider}
+            header={
+                <GlHeader
+                    title={
+                        <a {...routes.home().link}>
+                            <h1>header Title</h1>
+                        </a>
+                    }
+                    links={[
+                        {
+                            "label": "link 1",
+                            ...routes.page1().link,
+                        },
+                        {
+                            "label": "link 2",
+                            ...routes.page2().link,
+                        },
+                        {
+                            "label": "link 3",
+                            ...routes.page3().link,
+                        },
+                    ]}
+                    enableDarkModeSwitch={true}
+                    githubRepoUrl="https://github.com/torvalds/linux"
+                    githubButtonSize="large"
+                />
+            }
+            headerOptions={{
+                "position": "fixed",
+                "isRetracted": "smart",
+            }}
+            footer={
+                <GlFooter
+                    bottomDivContent="Licence M I T"
+                    email="email@email.com"
+                    phoneNumber="+33545345676"
+                    links={[
+                        {
+                            "label": "link 1",
+                            ...routes.page1().link,
+                        },
+                        {
+                            "label": "link 2",
+                            ...routes.page2().link,
+                        },
+                        {
+                            "label": "link 3",
+                            ...routes.page3().link,
+                        },
+                    ]}
+                />
+            }
+        >
+            {route.name === "home" && <Home />}
+            {route.name === "page1" && <Page1 />}
+            {route.name === "page2" && <Page2 />}
+            {route.name === "page3" && <Page3 />}
+        </GlTemplate>
+    );
 }
 
+//Wrap your app in the RouteProvider component in the router.ts file
 render(
-  <App />,
-  document.getElementById("root")
+    <RouteProvider>
+        <App />
+    </RouteProvider>,
+    document.getElementById("root"),
 );
