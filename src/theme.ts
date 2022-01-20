740f49975b75d75ca49f69c8962493fc8d4f729c
thieryw <williamalexthiery@hotmail.fr>
2022-01-20 14:06:08 +0100

    install prettier and update dependencys

--- src/theme.ts
+++ src/theme.ts
@@ -1,39 +1,38 @@
 import { createThemeProvider, defaultGetTypographyDesc } from "onyxia-ui";
 
 export const { ThemeProvider, useTheme } = createThemeProvider({
-	"getTypographyDesc": ({
-		browserFontSizeFactor, 
-		windowInnerHeight, 
-		windowInnerWidth
-	}) => {
+    "getTypographyDesc": ({
+        browserFontSizeFactor,
+        windowInnerHeight,
+        windowInnerWidth,
+    }) => {
+        const typographyDesc = defaultGetTypographyDesc({
+            browserFontSizeFactor,
+            windowInnerHeight,
+            windowInnerWidth,
+        });
 
-		const typographyDesc = defaultGetTypographyDesc({
-			browserFontSizeFactor,
-			windowInnerHeight, 
-			windowInnerWidth
-		})
-
-		return {
-			"fontFamily": '"Work Sans", sans-serif',
-			"rootFontSizePx": typographyDesc.rootFontSizePx,
-			"variants": {
-				...typographyDesc.variants,
-				"page heading": {
-					...typographyDesc.variants["page heading"],
-					"fontFamily": '"Playfair Display", serif'
-				},
-				"subtitle": {
-					...typographyDesc.variants["subtitle"],
-					"fontFamily": '"Open Sans", sans-serif'
-				},
-				"My title": {
-					"htmlComponent": "h1",
-					"fontWeight": "bold",
-					"fontSizeRem": 2,
-					"lineHeightRem": 2,
-					"fontFamily": '"Cinzel Decorative", cursive'
-				}
-			}
-		}
-	}
-}); 
+        return {
+            "fontFamily": '"Work Sans", sans-serif',
+            "rootFontSizePx": typographyDesc.rootFontSizePx,
+            "variants": {
+                ...typographyDesc.variants,
+                "page heading": {
+                    ...typographyDesc.variants["page heading"],
+                    "fontFamily": '"Playfair Display", serif',
+                },
+                "subtitle": {
+                    ...typographyDesc.variants["subtitle"],
+                    "fontFamily": '"Open Sans", sans-serif',
+                },
+                "My title": {
+                    "htmlComponent": "h1",
+                    "fontWeight": "bold",
+                    "fontSizeRem": 2,
+                    "lineHeightRem": 2,
+                    "fontFamily": '"Cinzel Decorative", cursive',
+                },
+            },
+        };
+    },
+});
